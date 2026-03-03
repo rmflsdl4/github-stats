17f0103d54aefc4bbc07eadce82d26bdba17158a
Flier <rmflsdl4@gmail.com>
2026-03-03 22:06:09 +0900

    Update supabaseClient.ts

--- server/supabaseClient.ts
+++ server/supabaseClient.ts
@@ -4,14 +4,13 @@ import {createClient} from '@supabase/supabase-js';
 import type {Database} from '../src/types/supabase';
 import {assert} from '../src/utils/assert';
 
-const {NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY} = process.env;
+const {NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY, SUPABASE_SERVICE_ROLE_KEY} = process.env;
 
 export const getSupabaseClient = (): SupabaseClient<Database> => {
   assert(NEXT_PUBLIC_SUPABASE_URL, 'NEXT_PUBLIC_SUPABASE_URL is not defined');
-  assert(NEXT_PUBLIC_SUPABASE_ANON_KEY, 'NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined');
-
+  assert(SUPABASE_SERVICE_ROLE_KEY || NEXT_PUBLIC_SUPABASE_ANON_KEY, 'Supabase key is not defined');
   return createClient<Database>(
     NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY || NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
 };
